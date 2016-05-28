5a185a3ad0dbed4ed66e55408f6fe63cc742bed9
Daniel <danielcarlossmd@gmail.com>
2016-05-28 12:59:21 -0300

    Hook device's return key to search method

--- myapp/www/js/components/movies/movies.directives.js
+++ myapp/www/js/components/movies/movies.directives.js
@@ -1,7 +1,8 @@
 'use strict';
 angular
   .module('movie')
-  .directive('moviePoster', moviePoster);
+  .directive('moviePoster', moviePoster)
+  .directive('ngEnter', ngEnter );
 
 function moviePoster() {
   return {
@@ -10,5 +11,24 @@ function moviePoster() {
       movie : '=movie'
     },
     templateUrl: 'js/components/movies/movie-poster.html'
+  };
+}
+
+//function that binds return key to a function
+//many thanks to this guy: https://gist.github.com/EpokK/5884263
+function ngEnter() {
+  return function(scope, element, attrs) {
+      //listen the keypress event
+      element.bind("keydown keypress", function(event) {
+          //13 = return key
+          if(event.which === 13) {
+              scope.$apply(function(){
+                //execute the fuction on ngEnter attr
+                scope.$eval(attrs.ngEnter);
+              });
+
+              event.preventDefault();
+          }
+      });
   };
 }
