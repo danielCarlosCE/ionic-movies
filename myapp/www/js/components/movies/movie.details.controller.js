22c63d89fdede5cce26783d152df25a44162d4ad
Daniel <danielcarlossmd@gmail.com>
2016-05-27 22:00:09 -0300

    Add ngCordova lib Add share with facebook feature

--- myapp/www/js/components/movies/movie.details.controller.js
+++ myapp/www/js/components/movies/movie.details.controller.js
@@ -3,6 +3,30 @@ angular
   .module('movie')
   .controller('MovieDetailsController', MovieDetailsController)
 
-function MovieDetailsController($scope, $stateParams) {
+function MovieDetailsController($scope, $stateParams, $cordovaSocialSharing) {
+  
+  //assign the attribute, passed by previous page, to scope variable
   $scope.movie = $stateParams.movie;
+
+  //share movie link via facebook
+  $scope.shareViaFacebook = function(){
+    //may be ignored by Facebook app
+    var optionalMessage = 'Check out this upcoming movie';
+
+    var link = 'https://www.themoviedb.org/movie/'+$scope.movie.id;
+    var image = null;
+
+    $cordovaSocialSharing
+      .shareViaFacebook(optionalMessage, image, link)
+      .then(
+        //success function
+        function(result) {
+          console.log(result);
+        },
+        //error function
+        function(err) {
+          console.error(err);
+        }
+      );
+  }
 }
